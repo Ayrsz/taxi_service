3d8528202175515895897b80c1c7df3bfb92be48
ayresz <matheusayressantos@gmail.com>
2025-07-14 23:30:50 -0300

    feat | setup routes main

--- routes/main.go
+++ routes/main.go
@@ -1,17 +1,21 @@
 package routes
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
+    "github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2/middleware/logger"
 )
 
+// SetupRoutes configura todas as rotas da aplicação.
 func SetupRoutes(app *fiber.App) {
+    // Cria um grupo de rotas na raiz '/' e adiciona um logger
+    // para vermos as requisições no console.
+    api := app.Group("/", logger.New())
 
-	api := app.Group("/", logger.New())
+    // Rota de "health check" para saber se a API está no ar.
+    api.Get("/health", func(c *fiber.Ctx) error {
+        return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "OK"})
+    })
 
-	api.Get("/health", func(c *fiber.Ctx) error {
-		return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "OK"})
-	})
-
-	DummyRoutes(api)
+    // Chama a função que configura as rotas de corrida.
+    SetupCorridaRoutes(api)
 }
